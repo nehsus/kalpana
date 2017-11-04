cd8efb57ac1db0796f9896f49d9ee6cb1c6b0b19
Sushen Kumar <sushenksharma@gmail.com>
2017-11-04 16:16:30 +0530

    synt

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, request, redirect
 from pymongo import MongoClient
 
-app=flask(__name__)
+app = Flask(__name__)
 client=MongoClient("localhost", 27015)
 
 @app.route("/")
 @app.route("/index")
-def index:
+def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    print(:27015)
+    print(':27015')
     app.run(debug=True)
 
